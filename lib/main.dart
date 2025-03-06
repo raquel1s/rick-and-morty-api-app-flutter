67eec0c14bc5d3c23564c77a919d79fc2b79102c
raquel1s <silva.raquel2903@gmail.com>
2025-03-06 19:19:28 -0300

    Adicionei a coluna com informações

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class RickAndMortyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Rick and Morty',
       theme: ThemeData(
         primarySwatch: Colors.teal,
--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -9,13 +9,48 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Character Details"),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(character.name), 
-      ),
-    );
+        appBar: AppBar(
+          title: const Text(
+            "Character Details",
+            style: TextStyle(fontSize: 28),
+          ),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                const SizedBox(height: 100),
+                  Text(
+                    character.name,
+                    style: const TextStyle(
+                        fontSize: 25, fontWeight: FontWeight.w700),
+                  ),
+                  const SizedBox(height: 25),
+                  Image.network(character.image),
+                  const SizedBox(height: 25),
+                  Text('Status: ${character.status}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 8),
+                  Text('Specie: ${character.species}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 8),
+                  Text('Gender: ${character.gender}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 8),
+                  Text('Origin: ${character.originName}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 8),
+                  Text('Location: ${character.locationName}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+          ),
+        );
   }
 }
