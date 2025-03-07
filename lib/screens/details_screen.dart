380b5b04f3c42249008ee62ba9198a91b7d85db4
raquel1s <silva.raquel2903@gmail.com>
2025-03-06 21:44:14 -0300

    aprimorando

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,56 +1,67 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:rm_app/models/character.dart';
 
-class DetailsScreen extends StatelessWidget {
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen(this.character, {super.key});
   final Character character;
 
-  const DetailsScreen({super.key, required this.character});
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
 
+class _DetailsScreenState extends State<DetailsScreen> {
   @override
   Widget build(BuildContext context) {
+    final character = widget.character;
+
     return Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            "Character Details",
-            style: TextStyle(fontSize: 28),
-          ),
-          centerTitle: true,
+      appBar: AppBar(
+        title: const Text(
+          "Character Details",
+          style: TextStyle(fontSize: 28),
         ),
-        body: Center(
-          child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                const SizedBox(height: 100),
-                  Text(
-                    character.name,
-                    style: const TextStyle(
-                        fontSize: 25, fontWeight: FontWeight.w700),
-                  ),
-                  const SizedBox(height: 25),
-                  Image.network(character.image),
-                  const SizedBox(height: 25),
-                  Text('Status: ${character.status}',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  const SizedBox(height: 8),
-                  Text('Specie: ${character.species}',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  const SizedBox(height: 8),
-                  Text('Gender: ${character.gender}',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  const SizedBox(height: 8),
-                  Text('Origin: ${character.originName}',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                  const SizedBox(height: 8),
-                  Text('Location: ${character.locationName}',
-                    style: const TextStyle(fontSize: 18),
-                  ),
-                ],
-              ),
-          ),
-        );
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(height: 100),
+            Text(
+              character.name,
+              style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+            ),
+            const SizedBox(height: 25),
+            CachedNetworkImage(imageUrl: character.image),
+            const SizedBox(height: 25),
+            Text(
+              'Status: ${character.status}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Specie: ${character.species}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Gender: ${character.gender}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Origin: ${character.originName}',
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Location: ${character.locationName}',
+              style: const TextStyle(fontSize: 18),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -41,35 +41,34 @@ class _HomeScreenState extends State<HomeScreen> {
               itemCount: characters.length,
               itemBuilder: (context, index) {
                 final character = characters[index];
-                return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => DetailsScreen(character: character),
-                        ),
-                      );
-                    },
-                    child: Card(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                        elevation: 4,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16)),
-                        child: ListTile(
-                          leading: CachedNetworkImage(
-                            imageUrl: character.image,
-                            width: 50,
-                            height: 50,
-                            fit: BoxFit.cover,
-                            placeholder: (context, url) =>
-                                const CircularProgressIndicator(),
-                            errorWidget: (context, url, error) =>
-                                const Icon(Icons.error),
+                return Card(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailsScreen(character),
                           ),
-                          title: Text(character.name),
-                          subtitle: Text('Status: ${character.status}'),
-                        )));
+                        );
+                      },
+                      leading: CachedNetworkImage(
+                        imageUrl: character.image,
+                        width: 50,
+                        height: 50,
+                        fit: BoxFit.cover,
+                        placeholder: (context, url) =>
+                            const CircularProgressIndicator(),
+                        errorWidget: (context, url, error) =>
+                            const Icon(Icons.error),
+                      ),
+                      title: Text(character.name),
+                      subtitle: Text('Status: ${character.status}'),
+                    ));
               },
             );
           }
